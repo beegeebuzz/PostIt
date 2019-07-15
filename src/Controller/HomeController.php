f5b3c444ff8c0737c88ab99517c488fdab9eba92
Guiderdoni Bastien <beegeebuzz@hotmail.fr>
2019-07-15 09:00:33 +0200

    assets install

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\PostIt;
+use App\Repository\PostItRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -11,11 +13,10 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index()
+    public function index(PostItRepository $postItRepository): Response
     {
-        $postIt = new PostIt();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'post_its' => $postItRepository->findAll(),
         ]);
     }
 }
